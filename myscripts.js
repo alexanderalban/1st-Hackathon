bbccf0fc5a5e91add668e8dd285dc4f5554fe907
alexanderalban <albanalexander@gmail.com>
2020-11-12 14:41:30 -0600

    added images and code works!

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,8 @@
-class HWord {
-    constructor(word, hint, encouragement) {
-        this.word = word;
-        this.hint = hint;
-        this.encouragement = encouragement;
-    }
-};
-
-const elephant = new HWord("elephant", "long nose and big ears", "You can do it!");
-const parrot = new HWord("parrot", "flies and can talk", "You've got this!");
-const octopus = new HWord("octopus", "has eight legs and swims", "You can do the thing!");
-
-// let wordArray = [elephant, parrot, octopus];
 let regularWordArray = ["elephant", "octopus", "parrot"];
 let wordArray = [];
 let blanksArray = [];
+let testArray = [];
+let turns = 6;
 
 let chooseWord = () => {
     return regularWordArray[Math.floor(Math.random() * regularWordArray.length)];
@@ -28,6 +17,7 @@ const createAnswerBlanks = () => {
         blanksArray[i] = "_";
     }
     document.getElementById("word-blank").innerHTML = blanksArray.join(' ');
+
     return blanksArray;
 };
 createAnswerBlanks();
@@ -41,29 +31,16 @@ const checkForWin = () => {
         if (wordArray[i] === guess) {
             blanksArray[i] = guess;
             document.getElementById("word-blank").innerHTML = blanksArray.join(' ');
+            console.log(blanksArray.join(' '));
         }
     }
-}
-
-
-
-// let alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// Come back to this! Input method for h-man
-// const alphaButtons = () => {
-//     myButtons = document.getElementById('alphabet-section');
-//     letters = document.createElement('ul');
-
-//     for (let i = 0; i < alphabetArray.length; i++) {
-//         letters.id = 'alphabet';
-//         list = document.createElement('li');
-//         list.id = 'letter';
-//         list.innerHTML = alphabetArray[i];
-//         //check letter against word
-//         myButtons.appendChild(letters);
-//         letters.appendChild(list);
-//     }
-// };
-
-// alphaButtons();
-
+    if (blanksArray.join(' ').includes(guess)) {
+        console.log("Great Guess!")
+    } else {
+        turns--;
+        document.getElementById("turns-remaining").innerHTML = "Turns Remaining " + turns;
+        if (turns === 0) {
+            alert("Out of turns! U R Ded.")
+        }
+    }
+};
